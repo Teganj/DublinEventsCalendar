125ad045d153edad3587c1cd84afe6a820f94b57
Tegan Jennings <46114237+Teganj@users.noreply.github.com>
2022-04-27 00:39:40 +0100

    Add files via upload

---
+++ src/main/java/com/mycompany/dublineventscalendar/DublinEventsCalendarClient/src/main/java/com/mycompany/dublineventscalendarclient/RunClient.java
@@ -0,0 +1,12 @@
+package com.mycompany.dublineventscalendarclient;
+
+/*
+ * @author Group A
+ */
+
+public class RunClient {
+    public static void main (String args[]){
+        ProvideOptionsGUI gui = new ProvideOptionsGUI();
+        gui.setVisible(true);
+    }
+}
